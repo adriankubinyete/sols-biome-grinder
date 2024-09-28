15d7b4f7aeba6e6421e7cd99a71db3ad91cecc8d
Adrian <adriankubinyete@outlook.com>
2024-09-28 14:05:08 -0300

    chore: init project

---
+++ main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// launch private
+// > private server url
+// > checking if game is already open (skip to check biome)
+// > confirm game is open
+
+// wait loadtime
+// > how to determine game is loaded
+
+// click start
+// > how to determine screen pos to cick
+
+// check biome, notify if needed
+// > determine where biome is located
+// > read text from biome
+// > if needed, notify
+
+// close private
+// > close game
+// > confirm game is closed
+
+// repeat
